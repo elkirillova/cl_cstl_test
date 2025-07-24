71af1b8908dac6aafef464f6f3e1df7c01c7ed12
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-25 00:43:44 +0400

    feat: add page draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-//import Image from "next/image";
-
 export default function Home() {
 	return (
 		<div className="container">
 			<p>Content</p>
+			<section id="section1" className="section" style={{height: '500px', border: '1px solid red'}}>Что ждет</section>
+			<section id="section2" className="section" style={{height: '500px', border: '1px solid blue'}}>Маршрут</section>
+			<section id="section3" className="section" style={{height: '500px', border: '1px solid green'}}>Бинго</section>
+			<section id="section4" className="section" style={{height: '500px', border: '1px solid pink'}}>FAQs</section>
 		</div>
 	)
 }
