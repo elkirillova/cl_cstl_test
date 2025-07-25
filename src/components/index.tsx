73c069545ea9373e600cae89ef029d06c25f660f
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-25 15:38:56 +0400

    feat: add hero section

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,11 @@
+// layout
 export { Header } from './Header/Header'
 export { Footer } from './Footer/Footer'
+
+// переиспользуемые компоненты
 export { Icon } from './ui/Icon/Icon'
 export { Overlay } from './ui/Overlay/Overlay'
 export { Portal } from './ui/Portal/Portal'
+
+// секции
+export { Hero } from './sections/Hero/Hero'
