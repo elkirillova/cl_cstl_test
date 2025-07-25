253fe5b1467e44ef782fdefe795de50ec45b30e1
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-25 15:47:12 +0400

    feat: implement Hero section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
+import { Hero } from '@/components'
+
 export default function Home() {
 	return (
-		<div className="container">
-			<p>Content</p>
-			<section id="section1" className="section" style={{height: '500px', border: '1px solid red'}}>Что ждет</section>
-			<section id="section2" className="section" style={{height: '500px', border: '1px solid blue'}}>Маршрут</section>
-			<section id="section3" className="section" style={{height: '500px', border: '1px solid green'}}>Бинго</section>
-			<section id="section4" className="section" style={{height: '500px', border: '1px solid pink'}}>FAQs</section>
-		</div>
+		<>
+			<Hero />
+			<section
+				id="section1"
+				className="section"
+				style={{ height: '500px', border: '1px solid red' }}
+			>
+				<div className="container">Что ждет</div>
+			</section>
+			<section
+				id="section2"
+				className="section"
+				style={{ height: '500px', border: '1px solid blue' }}
+			>
+				<div className="container">Маршрут</div>
+			</section>
+			<section
+				id="section3"
+				className="section"
+				style={{ height: '500px', border: '1px solid green' }}
+			>
+				<div className="container">Бинго</div>
+			</section>
+			<section
+				id="section4"
+				className="section"
+				style={{ height: '500px', border: '1px solid pink' }}
+			>
+				<div className="container">FAQs</div>
+			</section>
+		</>
 	)
 }
