e4fac1724f04ec97db54741963611c49a2940fbb
Elena Kirillova <el-ki-ri@yandex.ru>
2025-07-25 12:28:42 +0400

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,21 @@
 export const Footer = () => {
-	return <footer className="footer"><div className="container">Footer</div></footer>
+	return (
+		<footer className="footer">
+			<div className="container">
+				<div className="footer__wrapper">
+					<small>
+						&copy; 2024{' '}
+						<a
+							href="https://cloudcastle.ru/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="footer__link"
+						>
+							CLOUD CASTLE
+						</a>
+					</small>
+				</div>
+			</div>
+		</footer>
+	)
 }
